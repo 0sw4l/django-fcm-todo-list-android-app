f8e87adcd6b0ed6b516970e8b486bd239baba990
0sw4l <ioswxd@gmail.com>
2018-10-31 12:53:06 -0500

    first upload

---
+++ app/src/main/java/com/osw4l/django_fcm/utils/ClickHandler.java
@@ -0,0 +1,6 @@
+package com.osw4l.django_fcm.utils;
+
+
+public interface ClickHandler {
+    void onMyButtonClicked(final int position);
+}
---
+++ app/src/main/java/com/osw4l/django_fcm/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.osw4l.django_fcm.utils;
+
+public class Constants {
+    public final static String CURRENT_URL = "http://192.168.1.188:8000";
+    public static final String CHANNEL_ID = "TEST_DJANGO_FCM";
+}
